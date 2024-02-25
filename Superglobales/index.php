6143fc58469ce7bd3e274edb14867120eb8c3d7f
PharmaJava <boarding_87@hotmail.com>
2024-02-25 21:44:49 +0100

    Empezamos otro curso de PHP

---
+++ Superglobales/index.php
@@ -0,0 +1,16 @@
+<?php
+echo '<h1>';
+echo $_SERVER['SERVER_ADDR'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_NAME']; // Corrected key
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_SOFTWARE'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['REMOTE_ADDR'];
+?>
