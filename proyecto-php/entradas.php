942d6cec419132ee1d31f191eab7ff0a1b4fe0b1
PharmaJava <boarding_87@hotmail.com>
2024-03-07 18:41:52 +0100

    proyecto de web

--- proyecto-php/entradas.php
+++ proyecto-php/entradas.php
@@ -4,10 +4,10 @@
 		
 <!-- CAJA PRINCIPAL -->
 <div id="principal">
-	<h1>Ultimas entradas</h1>
+	<h1>Todas las entradas</h1>
 	
 	<?php 
-		$entradas = conseguirEntradas($db, true);
+		$entradas = conseguirEntradas($db);
 		if(!empty($entradas)):
 			while($entrada = mysqli_fetch_assoc($entradas)):
 	?>
@@ -24,10 +24,6 @@
 			endwhile;
 		endif;
 	?>
-
-	<div id="ver-todas">
-		<a href="entradas.php">Ver todas las entradas</a>
-	</div>
 </div> <!--fin principal-->
 			
 <?php require_once 'includes/pie.php'; ?>
---
+++ proyecto-php/includes/pie.php
@@ -0,0 +1,11 @@
+
+<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Antonio &copy; 2024</p>
+		</footer>
+		
+	</body>
+</html>
